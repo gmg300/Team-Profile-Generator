d85ac25b6b3387e24775999a9f41c21f034fc73a
Garrett <garrettgassensmith@gmail.com>
2020-04-11 15:42:30 -0500

    Converted team to a class put addEmployees inside, init function creates new team and calls addEmployees to fill out team

--- Develop/app.js
+++ Develop/app.js
@@ -4,109 +4,126 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-async function addEmployees(team) {
-    // Get employee data
-    let data = await inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Name:",
-        validate: function validateTitle(input) {
-          return input !== "";
-        },
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "ID Number:",
-        validate: function validateTitle(input) {
-          return input !== "";
-        },
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Email:",
-        validate: function validateTitle(input) {
-          return input !== "";
-        },
-      },
-      {
-        type: "list",
-        name: "title",
-        message: "Role:",
-        choices: ["Manager", "Engineer", "Intern"],
-        validate: function validateTitle(input) {
-          return input !== "";
-        },
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "Office Number:",
-        validate: function validateTitle(input) {
-          return input !== "";
-        },
-        when: function (input) {
-          if (input.title === "Manager") {
-            return true;
-          }
-          return false;
-        },
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Github Username:",
-        validate: function validateTitle(input) {
-          return input !== "";
-        },
-        when: function (input) {
-          if (input.title === "Engineer") {
-            return true;
-          }
-          return false;
-        },
-      },
-      {
-        type: "input",
-        name: "school",
-        message: "School:",
-        validate: function validateTitle(input) {
-          return input !== "";
-        },
-        when: function (input) {
-          if (input.title === "Intern") {
-            return true;
-          }
-          return false;
-        },
-      },
-    ]);
-    // Push data to team array
-    team.push(data);
-    // Loop if user wants to add more employees
-    let ask = await inquirer.prompt({
-        type: "confirm",
-        name: "again",
-        message: "Add more employees?"
-    });
-    if(ask.again) {
-        addEmployees(team);
-    } else {
-        console.log(team);
-        return team;
-    }
-
-
-
-
+class Team {
+  constructor() {
+    this.manager = {};
+    this.engineers = [];
+    this.interns = [];
   }
 
+  async addEmployees() {
+    try {
+      // Get employee data
+      let employee = await inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Name:",
+          validate: function validateTitle(input) {
+            return input !== "";
+          },
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "ID Number:",
+          validate: function validateTitle(input) {
+            return input !== "";
+          },
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Email:",
+          validate: function validateTitle(input) {
+            return input !== "";
+          },
+        },
+        {
+          type: "list",
+          name: "title",
+          message: "Role:",
+          choices: ["Manager", "Engineer", "Intern"],
+          validate: function validateTitle(input) {
+            return input !== "";
+          },
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "Office Number:",
+          validate: function validateTitle(input) {
+            return input !== "";
+          },
+          when: function (input) {
+            if (input.title === "Manager") {
+              return true;
+            }
+            return false;
+          },
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "Github Username:",
+          validate: function validateTitle(input) {
+            return input !== "";
+          },
+          when: function (input) {
+            if (input.title === "Engineer") {
+              return true;
+            }
+            return false;
+          },
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "School:",
+          validate: function validateTitle(input) {
+            return input !== "";
+          },
+          when: function (input) {
+            if (input.title === "Intern") {
+              return true;
+            }
+            return false;
+          },
+        },
+      ]);
+      // Push data to appropriate const
+      switch (employee.title) {
+        case "Manager":
+          this.manager = employee;
+          break;
+        case "Engineer":
+          this.engineers.push(employee);
+          break;
+        case "Intern":
+          this.interns.push(employee);
+          break;
+      }
+      // Loop if user wants to add more employees
+      let ask = await inquirer.prompt({
+        type: "confirm",
+        name: "again",
+        message: "Add more employees?",
+      });
+      if(ask.again) {
+        this.addEmployees();
+      } else {
+        console.log(this);
+        return;
+      }
+    } catch(err) {
+        console.error("Whoops!");
+    }
+  }
+}
+
 function init() {
-  const team = [];
-  addEmployees(team);
- 
+  const team = new Team();
+  team.addEmployees();
 }
 
 init();
